41b6ea4d5c27bb75984a24015ce16fee42cc5a2a
Andrew <mateikind@gmail.com>
2017-03-10 20:22:01 +0200

    update from server

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://79.115.78.9/prin_preajma/';
 
 /*
 |--------------------------------------------------------------------------
