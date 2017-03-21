a2102dca4ea29fe1e78881afc63542991ce55645
andrew1988 <mateikind@gmail.com>
2017-03-22 00:34:29 +0200

    find cat list bug

--- application/controllers/mockController.php
+++ application/controllers/mockController.php
@@ -5,8 +5,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class mockController extends MY_Controller {
 
     public function index() {
-        
-
         $this->load->view('inc/head');
         $this->load->view($this->generalViewsList['header']); //apeleaza proprietatea din MY_controller unde stabileste ce header incarca.
         $this->load->view('inc/main_search');
--- application/views/inc/cat_listing.php
+++ application/views/inc/cat_listing.php
@@ -4,7 +4,7 @@
             <?php foreach ($this->listCategorii as $categorie) { ?>
                 <div class="col-md-3 col-sm-6 col-xs-12">
                    <!-- <a href="<?php echo base_url('categorie/'.$categorie['cat_nume'].'/'.$categorie['cat_id'].'/0'); ?>"> -->
-                        <a href="<?php echo base_url('categorie'); ?>">
+                        <a href="<?php echo base_url('cath'); ?>">
                         <div class="button_cat"><?php echo  $categorie['cat_nume']; ?></div>
                     </a>
                 </div>
