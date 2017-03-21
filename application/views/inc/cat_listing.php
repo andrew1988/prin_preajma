66efa8114b033f6e481d4b45fadec61719462381
andrew1988 <mateikind@gmail.com>
2017-03-21 21:14:08 +0200

    category routes bug

--- application/views/inc/cat_listing.php
+++ application/views/inc/cat_listing.php
@@ -3,7 +3,7 @@
         <div class="row">
             <?php foreach ($this->listCategorii as $categorie) { ?>
                 <div class="col-md-3 col-sm-6 col-xs-12">
-                    <a href="<?php echo base_url('/categorie/'.$categorie['cat_nume'].'/'.$categorie['cat_id']).'/0'; ?>">
+                    <a href="<?php echo base_url('categorie/'.$categorie['cat_nume'].'/'.$categorie['cat_id']).'/0'; ?>">
                         <div class="button_cat"><?php echo  $categorie['cat_nume']; ?></div>
                     </a>
                 </div>
